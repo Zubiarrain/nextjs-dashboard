561dff1e6834548c2ec07e02ca7289f8a9366c00
Zubiarrain <zubiarrainnahuel@gmail.com>
2023-12-03 11:47:38 -0300

    feat(optimizing fonts and images): use of images and fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import styles from './ui/home.module.css';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
-
+import Image from 'next/image';
 import Link from 'next/link';
+import { lusitana } from './ui/fonts';
 
 export default function Page() {
   return (
@@ -16,7 +17,7 @@ export default function Page() {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+          <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
@@ -31,7 +32,21 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          {/* <img src="/hero-desktop.png" alt="Screenshot of the dashboard" /> */} {/* 500 kb */}
+          <Image 
+          src='/hero-desktop.png'
+          alt='Screenshot of the dashboard'
+          width={1000}
+          height={760}
+          className='hidden md:block'
+          /> {/* 171 KB, optimiza la imagen y el formato */}
+          <Image 
+          src='/hero-mobile.png'
+          alt='Screenshot of the dashboard'
+          width={560}
+          height={620}
+          className='block md:hidden'
+          />
         </div>
       </div>
     </main>
